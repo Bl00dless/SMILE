49e79d0097f701fe2d8b1074936e676b20ea831c
Darnier_Heros <sasha.bryko@gmail.com>
2015-03-26 21:18:08 +0200

    123125415ecyhf hgbfhkfy laib flbsjbliu eb

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -4,6 +4,34 @@
 $(document).ready(function() {
 
     $('.bxslider').bxSlider();
+    $("select").msDropDown();
+    $('input[type=text]').focus(function () {
+        $(this).css({border: '2px solid red', background: '#999'});
+    }).blur(function(){
+        var newVal = $(this).val().replace(/\s/g, '');
+        $(this).val(newVal);
+        if ($(this).val() == "" ) {
+            $(this).css({border: '1px solid red', background: '#FBACAC'})
+        }
+    }).change(function(){
+        if ($(this).val() == "" ){
+            $(this).css({border: '2px solid #999', background: '#FBACAC'});
+        }
+
+    });
+
+
+
+
+    $('#pass').blur(function(){
+        if ($(this).val().length <= 5){
+            alert ("Enter 6+ characters");
+        }
+    })
+
+
+
+
 
    $(".lang, .langpop").mouseenter(function()
    {
@@ -42,15 +70,14 @@ $(document).ready(function() {
 
 
 
-    var user = prompt('Whos here?','');
-    if (user !== null) {
-        if (user == 'admin'){
-        var pass = prompt('Password?', '');
-        if (pass =='blackoverlord') {
-            alert ('Welcome, my Lord!')
-        } else alert ('You are not prepared!')
-        } else if(user == null) alert ('Name yourself!')
-        else alert ('You are not the one i have waited for')
-    } else alert ('Do not come back!')
-
+    //var user = prompt('Whos here?','');
+    //if (user !== null) {
+    //    if (user == 'admin'){
+    //    var pass = prompt('Password?', '');
+    //    if (pass =='blackoverlord') {
+    //        alert ('Welcome, my Lord!')
+    //    } else alert ('You are not prepared!')
+    //    } else if(user == null) alert ('Name yourself!')
+    //    else alert ('You are not the one i have waited for')
+    //} else alert ('Do not come back!')
 });
